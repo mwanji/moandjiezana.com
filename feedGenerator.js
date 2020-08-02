96e71f77269721ad1c749fb29b02346d38365d60
Moandji Ezana <mwanji@gmail.com>
2020-08-02 19:45:40 +0200

    htmlRss writes file itself

--- feedGenerator.js
+++ feedGenerator.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
 const htmlRss = require('../html-rss/index.js');
 
-const rss = htmlRss({ baseUrl: 'https://moandjiezana.com' });
-fs.writeFileSync('feed.xml', rss);
+htmlRss({ baseUrl: 'https://moandjiezana.com' });
